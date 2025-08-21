69c187bdcaf6cec04d8c158389f205712badbadb
Dennis Manyagi <manyagi.dennis@gmail.com>
2025-08-21 15:04:11 -0700

    updated logo size

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
     <header className={`sticky top-0 bg-white z-50 border-b border-gray-300 text-black ${headerHeight}`}>
       <div className="container mx-auto flex items-center justify-between py-4 px-4 md:px-8">
         <Link href="/" className="flex items-center gap-3 font-bold uppercase tracking-widest">
-          <Image src="/images/logo.webp" alt="Manyagi Logo" width={100} height={50} loading="lazy" />
+          <Image src="/images/logo.svg" alt="Manyagi Logo" width={100} height={50} loading="lazy" />
         </Link>
         <nav className="flex flex-wrap gap-4 md:gap-6 items-center justify-center md:justify-end">
           <Link href="/" className="hover:text-yellow-500 transition">Home</Link>
